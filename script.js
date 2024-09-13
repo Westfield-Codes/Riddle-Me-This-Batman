6a51643ae2bea45b74d029a6dd3a11aa5ec97c89
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2024-09-13 13:04:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function bellRiddle(){
-  alert("Type the answer exactly, all lower case, no punctuation. Leave out \"Answer:\"");
   var answer = prompt("Why is an orange like a bell? Because they both must be ...");
-  if (answer == "peeled" ||  answer == "pealed") alert("Correct!");
-  else alert("Watch the video, reload, and try again.");
+  if (answer == "peeled" || answer == "pealed") alert("Correct!");
+  else alert("Wrong, Batman! Watch the video, and try again.");
 }
